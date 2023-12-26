950bf21278373780cd9c94be20969b32b7395c99
SteavenGamerYT <omarhanykasban706@gmail.com>
2023-12-26 17:46:34 +0200

    ah

--- content/files/minecraft.sh
+++ content/files/minecraft.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Function to display messages in a consistent format
 display_message() {
@@ -7,97 +8,111 @@ display_message() {
     echo "====================================="
 }
 
-# Check if the OS is Arch Linux
-if [ -f "/etc/arch-release" ]; then
-    display_message "Detected Arch Linux"
-
-    # List of packages to uninstall
-    packages_to_uninstall=(
-        prismlauncher
-        prismlauncher-git
-        prismlauncher-qt5
-        prismlauncher-qt5-bin
-        prismlauncher-qt5-git
-        prismlauncher-bin
-        multimc-bin
-        multimc-git
-        multimc5
-        pollymc-qt5-git 
-        polymc-qt5-git
-        polymc-qt5 
-        polymc-qt5-bin
-        polymc-bin
-        polymc-git
-        polymc
-    )
-
-    # Uninstall listed packages
-    for package in "${packages_to_uninstall[@]}"; do
-        if pacman -Qs "^$package$" &>/dev/null; then
-            display_message "Uninstalling $package Removing the other version of Prism Luncher"
-            yes | sudo pacman -Rns "$package" --noconfirm
-        fi
-    done
-
-    # Check if yay is available
-    if command -v yay &> /dev/null; then
-        display_message "Using yay to install prismlauncher-bin"
-        yay -S prismlauncher-bin --noconfirm
-    elif command -v paru &> /dev/null; then
-        display_message "Using paru to install prismlauncher-bin"
-        paru -S prismlauncher-bin --noconfirm
-    else
-        display_message "yay and paru not found, falling back to manual installation process"
-
-        # Clone and install prismlauncher-bin
-        git clone https://aur.archlinux.org/prismlauncher-bin.git
-        cd prismlauncher-bin || exit
-        yes | makepkg -si --noconfirm
-        cd .. || exit
-    fi
-fi
-
-
-# Check if the OS is Fedora
-if [ -f "/etc/fedora-release" ]; then
-    display_message "Detected Fedora"
+# Detect Linux distribution
+if command -v zypper &> /dev/null; then
+    # openSUSE
+    sudo zypper prismlauncher -y
+elif command -v dnf &> /dev/null; then
+    # Fedora
     sudo dnf copr disable polymc/polymc -y
     sudo dnf remove polymc -y
     sudo dnf copr enable g3tchoo/prismlauncher -y
     sudo dnf install prismlauncher -y
-fi
-
-# Check if the OS is Debian or Ubuntu
-if [ -f "/etc/debian_version" ] || [ -f "/etc/lsb-release" ]; then
-    display_message "Detected Debian or Ubuntu"
-    
-    # Use nala if available, fallback to apt, then apt-get
-    if command -v nala &> /dev/null; then
-        package_manager="nala"
-    elif command -v apt &> /dev/null; then
-        package_manager="apt"
-    elif command -v apt-get &> /dev/null; then
-        package_manager="apt-get"
-    else
-        display_message "No package manager found. Exiting."
-        exit 1
-    fi
-    
-    display_message "Using package manager: $package_manager"
-
-    # Install prerequisites
-    $package_manager update
-    $package_manager install -y curl gnupg
-
-    # Configure prebuilt-mpr repository
+elif command -v apt &> /dev/null; then
+    # Debian or Ubuntu
+    sudo apt update
+    sudo apt install -y curl gnupg
     curl -q 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
-    echo "deb [signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/$package_manager/sources.list.d/prebuilt-mpr.list
-
-    # Install prismlauncher
-    $package_manager update
-    yes | $package_manager remove multimc        # Automatically confirm the removal
-    yes | $package_manager remove prismlauncher  # Automatically confirm the removal
-    $package_manager install prismlauncher -y
+   # echo "deb [signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
+    echo "deb [signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr bookworm" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
+    sudo apt update
+    sudo apt purge multimc -y || true
+    sudo apt purge prismlauncher -y || true
+    sudo apt install prismlauncher -y
+elif command -v yay &> /dev/null; then
+    # Arch Linux
+    sudo pacman -Rns --noconfirm prismlauncher || true
+    sudo pacman -Rns --noconfirm prismlauncher-git || true
+    sudo pacman -Rns --noconfirm prismlauncher-qt5 || true
+    sudo pacman -Rns --noconfirm prismlauncher-qt5-bin || true
+    sudo pacman -Rns --noconfirm prismlauncher-qt5-git || true
+    sudo pacman -Rns --noconfirm prismlauncher-bin || true
+    sudo pacman -Rns --noconfirm multimc-bin || true
+    sudo pacman -Rns --noconfirm multimc-git || true
+    sudo pacman -Rns --noconfirm multimc5 || true
+    sudo pacman -Rns --noconfirm pollymc-qt5-git || true
+    sudo pacman -Rns --noconfirm polymc-qt5-git || true
+    sudo pacman -Rns --noconfirm polymc-qt5 || true
+    sudo pacman -Rns --noconfirm polymc-qt5-bin || true
+    sudo pacman -Rns --noconfirm polymc-bin || true
+    sudo pacman -Rns --noconfirm polymc-git || true
+    sudo pacman -Rns --noconfirm polymc || true
+    yay -S --noconfirm prismlauncher
+elif command -v paru &> /dev/null; then
+    # Arch Linux
+    sudo pacman -Rns --noconfirm prismlauncher || true
+    sudo pacman -Rns --noconfirm prismlauncher-git || true
+    sudo pacman -Rns --noconfirm prismlauncher-qt5 || true
+    sudo pacman -Rns --noconfirm prismlauncher-qt5-bin || true
+    sudo pacman -Rns --noconfirm prismlauncher-qt5-git || true
+    sudo pacman -Rns --noconfirm prismlauncher-bin || true
+    sudo pacman -Rns --noconfirm multimc-bin || true
+    sudo pacman -Rns --noconfirm multimc-git || true
+    sudo pacman -Rns --noconfirm multimc5 || true
+    sudo pacman -Rns --noconfirm pollymc-qt5-git || true
+    sudo pacman -Rns --noconfirm polymc-qt5-git || true
+    sudo pacman -Rns --noconfirm polymc-qt5 || true
+    sudo pacman -Rns --noconfirm polymc-qt5-bin || true
+    sudo pacman -Rns --noconfirm polymc-bin || true
+    sudo pacman -Rns --noconfirm polymc-git || true
+    sudo pacman -Rns --noconfirm polymc || true
+    paru -S --noconfirm prismlauncher
+else
+    echo "Unsupported distribution. Please install flatpak and curl manually."
+    exit 1
 fi
+
+display_message "Prism Luncher was installed successfully."
+
+# Downloads Minecraft 1.20.4 with perfromace modes and 1.20.1 with optfine
+# Detect the user's home directory
+user_home=$(eval echo ~"$USER")
+
+# Define the URL for downloading minecraft.tar.gz
+minecraft_url="https://www.dropbox.com/scl/fi/whwsruso1dk6y1kyndpca/minecraft.tar.gz?rlkey=9npj1olyt0l0azya1rxt4njwe&dl=1"
+
+# Define the extraction directory
+extract_dir="$user_home/.local/share/PrismLauncher"
+
+# Download the minecraft.tar.gz file
+echo "Downloading Minecraft..."
+wget -O minecraft.tar.gz "$minecraft_url"
+
+# Check if the download was successful
+if [ $? -eq 0 ]; then
+    echo "Minecraft has been successfully downloaded."
+else
+    echo "Error: Failed to download Minecraft. Please check the URL and try again."
+    exit 1
+fi
+
+# Check if the extraction directory exists, create it if not
+if [ ! -d "$extract_dir" ]; then
+    mkdir -p "$extract_dir"
+fi
+
+# Extract the tar.gz file to the specified directory
+tar -xzf minecraft.tar.gz -C "$extract_dir"
+
+# Check if the extraction was successful
+if [ $? -eq 0 ]; then
+    echo "Minecraft has been successfully extracted to $extract_dir"
+else
+    echo "Error: Failed to extract Minecraft to $extract_dir"
+    exit 1
+fi
+
+# Clean up: Remove the downloaded zip file
+rm -f minecraft.tar.gz
 
 display_message "Minecraft was installed successfully."
---
+++ content/posts/games/minecraft.md
@@ -0,0 +1,27 @@
+---
+title: "Minecraft Repack (Steaven Repacks)"
+date: 2023-12-20
+url: /minecraft/
+image: images/2023-thumbs/my-first-post.jpg
+categories:
+  - Linux
+  - Windows
+  - Games
+tags:
+  - Ubuntu
+  - Arch Linux
+  - Windows
+draft: false
+---
+## Linux
+`bash <(wget -qO- raw.githubusercontent.com/SteavenGamerYT/Website/main/content/files/minecraft.sh)`
+
+TRUST ME ITS WORKING ON 
+
+***ARCH LINUX BTW***
+
+***UBUNTU BTW***
+
+idk others
+
+still working on getting others working
